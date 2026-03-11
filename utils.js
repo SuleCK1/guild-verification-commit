e9be8d9a10012a33d01f314a1b83eb3077779410
SuleCK1 <151072867+SuleCK1@users.noreply.github.com>
2026-03-11 23:07:32 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T20:07:19.465Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-11T20:07:32.557Z
+Commit: Patch API endpoint error handling
