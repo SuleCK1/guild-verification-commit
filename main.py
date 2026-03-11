ba2cd0767a6bb6799378e0f649ebf056f7e392cf
SuleCK1 <151072867+SuleCK1@users.noreply.github.com>
2026-03-11 23:05:49 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T20:04:07.838Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-11T20:05:48.618Z
+Commit: Fix memory leak in event handlers
