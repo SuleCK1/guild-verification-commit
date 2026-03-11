17e88badf4657f80242c4418a6e11fe9917d3497
SuleCK1 <151072867+SuleCK1@users.noreply.github.com>
2026-03-11 23:02:18 +0300

    Patch API endpoint error handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T20:01:46.936Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-11T20:02:17.671Z
+Commit: Patch API endpoint error handling
